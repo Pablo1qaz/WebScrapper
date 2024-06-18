1b048fe29b5974a852f79588639b09a0ec8ca8fc
Pablo1qaz <147703596+Pablo1qaz@users.noreply.github.com>
2024-06-18 14:00:22 +0200

    #

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,20 +1,44 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scraper {
 
-    public static void main(String[] args){
-        String url = "https://www.flashscore.pl";
+    private String team;
+    private String score;
 
+    public String toString(){
+        return team + score;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Document document;
         try {
-            Document document = Jsoup.connect(url).get();
-            Elements matches = document.select("#g_1_Q9YPy46c");
+            document = Jsoup.connect("https://www.flashscore.pl/tabela/ABkrguJ9/EcpQtcVi/#/EcpQtcVi/table").get();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        List<Scraper> products = new ArrayList<>();
+        Elements matches = document.select(".tableWrapper");
+
+        for (Element productElement : matches) {
+            Scraper product = new Scraper();
+
+
+
+
+        }
     }
-}
+    }
