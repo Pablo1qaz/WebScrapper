662e95fbd5789b2a5abc90d844d884c62804919a
Pablo1qaz <147703596+Pablo1qaz@users.noreply.github.com>
2024-06-23 13:26:05 +0200

    .

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,43 +1,44 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-import java.io.IOException;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
 
 public class Scraper {
 
-    private String team;
-    private String score;
-
-    public String toString(){
-        return team + score;
-
-    }
-
-
-
     public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 
-        Document document;
-        try {
-            document = Jsoup.connect("https://www.flashscore.pl/tabela/ABkrguJ9/EcpQtcVi/#/EcpQtcVi/table").get();
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.flashscore.pl/tabela/ABkrguJ9/EcpQtcVi/#/EcpQtcVi/table");
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("tournament-table-tabs-and-content")));
+
+        WebElement tableContents = driver.findElement(By.id("tournament-table-tabs-and-content"));
+        List<WebElement> rows = tableContents.findElements(By.cssSelector(".ui-table__body .ui-table__row"));
+
+        if (rows.isEmpty()) {
+            System.out.println("No rows found");
         }
 
-        List<Scraper> products = new ArrayList<>();
-        Elements matches = document.select(".tableWrapper");
-
-        for (Element productElement : matches) {
-            Scraper product = new Scraper();
-
-
-
+        for (WebElement row : rows) {
+            List<WebElement> columns = row.findElements(By.cssSelector(".table__cell"));
+            if (columns.isEmpty()) {
+                System.out.println("No columns found in row");
+            }
+            for (WebElement column : columns) {
+                System.out.print(column.getText() + " ");
+            }
+            System.out.println();
         }
+
+        driver.quit();
     }
-    }
+}
